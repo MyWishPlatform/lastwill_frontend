f00dabf8c99aa3f9254897726851c97f5d33b236
Kubas <dru.kubasov@gmail.com>
2017-08-10 18:57:51 +0300

    fix pre tag formatting

--- src/app/new-contract/step-3/step-3.component.ts
+++ src/app/new-contract/step-3/step-3.component.ts
@@ -32,34 +32,34 @@ export class Step3Component implements OnInit {
       'heirs': this.wallet['destination'],
       'conditions': this.wallet['conditions']
     };
-    ///////
-    //const contract = '// —---------- MODIFIERS —---------\n' +
-    //  '    modifier onlyTarget() {\n' +
-    //  '        require(isTarget());\n' +
-    //  '        _;\n' +
-    //  '    }\n' +
-    //  '\n' +
-    //  '    modifier onlyAdmin() {\n' +
-    //  '        require(lastWillAccount == msg.sender);\n' +
-    //  '        _;\n' +
-    //  '    }\n' +
-    //  '\n' +
-    //  '    modifier onlyTargetOrAdmin() {\n' +
-    //  '        require(targetUser == msg.sender || lastWillAccount == msg.sender);\n' +
-    //  '        _;\n' +
-    //  '    }\n' +
-    //  '\n' +
-    //  '}';
-    //this.contractCreated.emit(contract);
-    //this.step = 'contract';
-    //this.nextStep.emit(this.step);
-    this.contractRest.createContract(newContract).subscribe((res) => {
-      this.contractCreated.emit(res.contract);
-      this.step = 'contract';
-      this.nextStep.emit(this.step);
-    }, (err) => {
-      console.error(err);
-    } );
+    /////
+    const contract = '// —---------- MODIFIERS —---------\n' +
+      '    modifier onlyTarget() {\n' +
+      '        require(isTarget());\n' +
+      '        _;\n' +
+      '    }\n' +
+      '\n' +
+      '    modifier onlyAdmin() {\n' +
+      '        require(lastWillAccount == msg.sender);\n' +
+      '        _;\n' +
+      '    }\n' +
+      '\n' +
+      '    modifier onlyTargetOrAdmin() {\n' +
+      '        require(targetUser == msg.sender || lastWillAccount == msg.sender);\n' +
+      '        _;\n' +
+      '    }\n' +
+      '\n' +
+      '}';
+    this.contractCreated.emit(contract);
+    this.step = 'contract';
+    this.nextStep.emit(this.step);
+    //this.contractRest.createContract(newContract).subscribe((res) => {
+    //  this.contractCreated.emit(res.contract);
+    //  this.step = 'contract';
+    //  this.nextStep.emit(this.step);
+    //}, (err) => {
+    //  console.error(err);
+    //} );
   }
   normalizeConditions() {
     this.conditionsNormalized.duration = this.conditions.duration * this.SECONDS;
