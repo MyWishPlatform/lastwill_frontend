10fbfaa25743039b48575297b4ef28dc3de66576
Kubas <dru.kubasov@gmail.com>
2017-08-11 13:37:00 +0300

    parent component testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,35 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { NewContractComponent } from './new-contract/new-contract.component';
+import { FormsModule } from '@angular/forms';
+import { Step1Component } from './new-contract/step-1/step-1.component';
+import { Step2Component } from './new-contract/step-2/step-2.component';
+import { Step3Component } from './new-contract/step-3/step-3.component';
+import { Step4Component } from './new-contract/step-4/step-4.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        UserLoginComponent,
+        NewContractComponent,
+        Step1Component,
+        Step2Component,
+        Step3Component,
+        Step4Component
       ],
+      imports: [
+        AppRoutingModule,
+        RouterModule,
+        FormsModule,
+        RouterTestingModule
+      ]
     }).compileComponents();
   }));
 
@@ -17,16 +39,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have a title 'Last Will'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('Last Will');
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render link with image', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('a img.nav--logo'));
   }));
 });
--- src/app/new-contract/new-contract.component.spec.ts
+++ src/app/new-contract/new-contract.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NewContractComponent } from './new-contract.component';
+import { Step1Component } from './step-1/step-1.component';
+import { Step2Component } from './step-2/step-2.component';
+import { Step3Component } from './step-3/step-3.component';
+import { Step4Component } from './step-4/step-4.component';
+import { FormsModule } from '@angular/forms';
+import { ContractServiceService } from './contract-service.service';
+import { RestApiService } from '../services/rest-api.service';
+import { HttpModule } from '@angular/http';
 
 describe('NewContractComponent', () => {
   let component: NewContractComponent;
@@ -8,7 +16,21 @@ describe('NewContractComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NewContractComponent ]
+      declarations: [
+        NewContractComponent,
+        Step1Component,
+        Step2Component,
+        Step3Component,
+        Step4Component
+      ],
+      imports: [
+        FormsModule,
+        HttpModule
+      ],
+      providers: [
+        RestApiService,
+        ContractServiceService
+      ]
     })
     .compileComponents();
   }));
