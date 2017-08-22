b6b3145d359cbc6f0c2e8e3b2eca2ad0f13cc033
Kubas <dru.kubasov@gmail.com>
2017-08-22 18:49:05 +0300

    fix hash routing problem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {RouterModule, Routes} from '@angular/router';
+import { NavigationStart, Router, RouterModule, Routes } from '@angular/router';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { AuthGuardService } from './services/auth-guard.service';
 import { NewContractComponent } from './new-contract/new-contract.component';
@@ -10,6 +9,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: UserLoginComponent },
   { path: 'contract', component: NewContractComponent, canActivate: [AuthGuardService] },
+  { path: '**', redirectTo: '/login', pathMatch: 'full' },
 ];
 
 @NgModule({
@@ -19,4 +19,14 @@ const routes: Routes = [
   exports: [ RouterModule ],
   declarations: []
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  constructor(private router: Router) {
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        if (event.url.indexOf('/#') === 0) {
+          router.navigate([event.url.slice(2)]);
+        }
+      }
+    });
+  }
+}
